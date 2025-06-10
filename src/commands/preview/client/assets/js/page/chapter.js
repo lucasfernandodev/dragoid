0b11d2a8364829e9297becd89a7ffea56a3a0dd7
Lucas Fernando <lucasfernando.dev@gmail.com>
2025-06-10 00:57:26 -0300

    feat: Implement navigation shortcut using keyboard

--- src/commands/preview/client/assets/js/page/chapter.js
+++ src/commands/preview/client/assets/js/page/chapter.js
@@ -3,6 +3,8 @@ import { modalChapterStyle } from "../components/modal-chapter-style/index.js";
 import { modalReplacement } from "../components/modal-replace/index.js";
 import { applyUserStyles } from "../core/chapter-style-setting/apply-user-style.js";
 import { applyUserReplacements } from "../core/replacement/apply-user-replacements.js";
+import { ShortcutKeyboardNavigation } from "../core/shortcuts/keyboard/navigation.js";
+
 import { onTouchGesture } from "../core/shortcuts/on-touch-gesture.js";
 import { isMobile } from "../utils/is-Mobile.js";
 
@@ -17,9 +19,21 @@ const initChapterPage = async () => {
 
 
 
-  // === Aplica customizações do usuário no capítulo ===
+  // === User Tools ===
   applyUserReplacements()
   applyUserStyles()
+
+
+  // Shortcut Keyboard Navigation
+  const prevChapterLink = document.querySelector('.nav-prev')
+  const nextChapterLink = document.querySelector('.nav-next')
+  const prevId = prevChapterLink.dataset.id
+  const nextId = nextChapterLink.dataset.id
+
+  new ShortcutKeyboardNavigation({
+    prev_chapter: prevId,
+    next_chapter: nextId
+  })
 
 
   // Handle with show/hidden floating navigation in mobile devices
