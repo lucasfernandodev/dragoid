02522b3d5c5c33380122e3841597671ad4e57dc2
Lucas Fernando <lucasfernando.dev@gmail.com>
2025-06-16 10:04:11 -0300

    refactor: remove unused file

--- src/dragoid.ts
+++ src/dragoid.ts
@@ -3,8 +3,7 @@
 import yargs from "yargs";
 import { errorHandle } from "./errors/error-handle.ts";
 import { Download } from "./commands/download/index.ts";
-import { Preview } from "./commands/preview/index.ts";
-import type { TypeCommandPreviewArgs } from "./types/command-preview-args.ts";
+import { Preview } from "./commands/preview/index.ts"; 
 import { logger } from "./utils/logger.ts";
 import { BotReadNovelFull } from './core/bots/readnovelfull/index.ts';
 import { Bot69Shuba } from './core/bots/69shuba/index.ts';
@@ -12,9 +11,9 @@ import { language } from "./core/configurations.ts";
 import { getCurrentVersion } from "./utils/get-current-version.ts";
 import { BotNovelBin } from "./core/bots/novelbin/index.ts";
 import type { DownloadArgs } from "./commands/download/options.ts";
+import type { PreviewArgs } from "./commands/preview/options.ts";
 
 const _yargs = yargs(process.argv.slice(2))
-
 
 
 const bots = [
@@ -36,7 +35,7 @@ _yargs.command<DownloadArgs>(
 )
 
 // Mode Preview
-_yargs.command<TypeCommandPreviewArgs>(
+_yargs.command<PreviewArgs>(
   previewClient.commandEntry,
   previewClient.describe,
   previewClient.parserInputs,
