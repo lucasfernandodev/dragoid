f8f4068f40cbceacd13df7ce17217aa1a2f42ae2
Lucas Fernando <lucasfernando.dev@gmail.com>
2025-06-27 14:38:04 -0300

    feat(bot): Improving collect data validation

--- src/core/bots/readnovelfull/_get-chapter.ts
+++ src/core/bots/readnovelfull/_get-chapter.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {type CheerioAPI, load} from 'cheerio';
-import { BotError } from '../../../errors/bot-error.ts';
+import { BotError } from '../../../errors/bot-error.ts'; 
 
 export const readnovelfullGetChapter = async (url: string) => {
 
@@ -20,8 +20,14 @@ export const readnovelfullGetChapter = async (url: string) => {
   
   const $ = load(response.data)
 
-  return {
+  const chapter = {
     title: getTitle($),
     content: getContent($)
   }
+
+  if(!chapter.title.trim() && chapter.content.length === 0){
+    throw new BotError('No information could be retrieved.\n- Check if the url is correct\n- Check if the website is working')
+  }
+
+  return chapter
 }
